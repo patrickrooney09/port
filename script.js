009d6e4a34ccca31a3ae1faf7b54da74311da13b
Patrick Rooney <patrickrooney09@gmail.com>
2023-05-02 15:56:43 -0400

    added some styling for carousel, everything still functional

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 // first thing I want to do: add elements via js
+let track;
+let slides;
+let slideWidth;
+let dotsNav;
+let dots;
+
+// const nextButton = document.querySelector(".carousel_button--right");
+// const prevButton = document.querySelector(".carousel_button--left");
+
+const setSlidePosition = (slide, index) => {
+  slide.style.left = slideWidth * index + "px";
+};
+
 const rehearseFileNames = [
   "project-images-and-gifs/rehearse-home-page.png",
   "project-images-and-gifs/rehearse-build-scene.png",
@@ -10,14 +23,45 @@ const diagonAlleyFileNames = [
   "project-images-and-gifs/diagon-alley-cart-and-checkout.png",
   "project-images-and-gifs/diagon-alley-shop.png",
 ];
+
+const chewsyFileNames = ["project-images-and-gifs/chewsy-pages.png"];
+
 const createCarousel = (fileNames) => {
+  if (document.querySelector(".carousel_track") !== null) {
+    document.querySelector(".carousel_track").remove();
+    document.querySelector(".carousel_nav").remove();
+    console.log(document.querySelector(".carousel_track"));
+    console.log(document.querySelector(".carousel_nav"));
+    document.querySelector(".carousel_button--left").remove();
+    document.querySelector(".carousel_button--right").remove();
+  }
+
+  const prevButtonImage = document.createElement("img");
+  prevButtonImage.src = "icons/png/back.png";
+  const nextButtonImage = document.createElement("img");
+  nextButtonImage.src = "icons/png/next.png";
+
+  const prevButton = document.createElement("button");
+  prevButton.setAttribute("class", "carousel_button");
+  prevButton.classList.add("carousel_button--left");
+  prevButton.classList.add("is-hidden");
+  prevButton.appendChild(prevButtonImage);
+
+  const nextButton = document.createElement("button");
+  nextButton.setAttribute("class", "carousel_button");
+  nextButton.classList.add("carousel_button--right");
+  nextButton.appendChild(nextButtonImage);
+
   const carousel = document.querySelector(".carousel_track-container");
+  document.querySelector(".carousel").appendChild(prevButton);
+
   const carouselTrack = document.createElement("ul");
   carouselTrack.setAttribute("class", "carousel_track");
   carousel.appendChild(carouselTrack);
 
   const carouselNav = document.createElement("div");
   carouselNav.setAttribute("class", "carousel_nav");
+  document.querySelector(".carousel").appendChild(nextButton);
   document.querySelector(".carousel").appendChild(carouselNav);
 
   fileNames.forEach((currentImage, index) => {
@@ -30,7 +74,6 @@ const createCarousel = (fileNames) => {
 
     carouselSlide.setAttribute("class", "carousel_slide");
     if (index === 0) {
-      // carouselSlide.setAttribute("class", "current-slide");
       carouselSlide.classList.add("current-slide");
     }
     carouselSlide.appendChild(img);
@@ -45,21 +88,56 @@ const createCarousel = (fileNames) => {
     }
     carouselNav.appendChild(carouselDot);
   });
+
+  track = document.querySelector(".carousel_track");
+  slides = Array.from(track.children);
+  slideWidth = slides[0].getBoundingClientRect().width;
+  slides.forEach(setSlidePosition);
+  dotsNav = document.querySelector(".carousel_nav");
+  dots = Array.from(dotsNav.children);
+
+  dotsNav.addEventListener("click", (e) => {
+    // what did we click on
+    console.log("hello");
+    const targetDot = e.target.closest("button");
+    if (!targetDot) return;
+
+    const currentSlide = track.querySelector(".current-slide");
+    const currentDot = dotsNav.querySelector(".current-slide");
+    const targetIndex = dots.findIndex((dot) => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+
+    hideShowArrows(slides, prevButton, nextButton, targetIndex);
+  });
+
+  nextButton.addEventListener("click", (e) => {
+    console.log("hello");
+    let currentSlide = track.querySelector(".current-slide");
+    let nextSlide = currentSlide.nextElementSibling;
+    const currentDot = dotsNav.querySelector(".current-slide");
+    const nextDot = currentDot.nextElementSibling;
+    const nextIndex = slides.findIndex((slide) => slide === nextSlide);
+
+    moveToSlide(track, currentSlide, nextSlide);
+    updateDots(currentDot, nextDot);
+    hideShowArrows(slides, prevButton, nextButton, nextIndex);
+  });
+
+  prevButton.addEventListener("click", (e) => {
+    let currentSlide = track.querySelector(".current-slide");
+    let previousSlide = currentSlide.previousElementSibling;
+    const currentDot = dotsNav.querySelector(".current-slide");
+    const previousDot = currentDot.previousElementSibling;
+    const previousIndex = slides.findIndex((slide) => slide === previousSlide);
+
+    moveToSlide(track, currentSlide, previousSlide);
+    updateDots(currentDot, previousDot);
+    hideShowArrows(slides, prevButton, nextButton, previousIndex);
+  });
 };
 createCarousel(rehearseFileNames);
-
-let track = document.querySelector(".carousel_track");
-let slides = Array.from(track.children);
-let slideWidth = slides[0].getBoundingClientRect().width;
-const setSlidePosition = (slide, index) => {
-  slide.style.left = slideWidth * index + "px";
-};
-slides.forEach(setSlidePosition);
-
-const nextButton = document.querySelector(".carousel_button--right");
-const prevButton = document.querySelector(".carousel_button--left");
-const dotsNav = document.querySelector(".carousel_nav");
-const dots = Array.from(dotsNav.children);
 
 function darkMode() {
   let element = document.getElementsByClassName("opening-page");
@@ -81,6 +159,12 @@ const projectButtons = [
   currentButton.addEventListener("click", () => {
     if (currentButton.innerText === "Diagon Alley Online") {
       createCarousel(diagonAlleyFileNames);
+    }
+    if (currentButton.innerText === "Rehearse") {
+      createCarousel(rehearseFileNames);
+    }
+    if (currentButton.innerText === "Chewsy") {
+      createCarousel(chewsyFileNames);
     }
   });
 });
@@ -111,62 +195,44 @@ const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
   }
 };
 // when i click left move slides to left
-prevButton.addEventListener("click", (e) => {
-  let currentSlide = track.querySelector(".current-slide");
-  let previousSlide = currentSlide.previousElementSibling;
-  const currentDot = dotsNav.querySelector(".current-slide");
-  const previousDot = currentDot.previousElementSibling;
-  const previousIndex = slides.findIndex((slide) => slide === previousSlide);
+// prevButton.addEventListener("click", (e) => {
+//   let currentSlide = track.querySelector(".current-slide");
+//   let previousSlide = currentSlide.previousElementSibling;
+//   const currentDot = dotsNav.querySelector(".current-slide");
+//   const previousDot = currentDot.previousElementSibling;
+//   const previousIndex = slides.findIndex((slide) => slide === previousSlide);
 
-  moveToSlide(track, currentSlide, previousSlide);
-  updateDots(currentDot, previousDot);
-  hideShowArrows(slides, prevButton, nextButton, previousIndex);
-});
-// when i click right move slides to the right
-nextButton.addEventListener("click", (e) => {
-  console.log("hello");
-  let currentSlide = track.querySelector(".current-slide");
-  let nextSlide = currentSlide.nextElementSibling;
-  const currentDot = dotsNav.querySelector(".current-slide");
-  const nextDot = currentDot.nextElementSibling;
-  const nextIndex = slides.findIndex((slide) => slide === nextSlide);
+//   moveToSlide(track, currentSlide, previousSlide);
+//   updateDots(currentDot, previousDot);
+//   hideShowArrows(slides, prevButton, nextButton, previousIndex);
+// });
+// // when i click right move slides to the right
+// nextButton.addEventListener("click", (e) => {
+//   console.log("hello");
+//   let currentSlide = track.querySelector(".current-slide");
+//   let nextSlide = currentSlide.nextElementSibling;
+//   const currentDot = dotsNav.querySelector(".current-slide");
+//   const nextDot = currentDot.nextElementSibling;
+//   const nextIndex = slides.findIndex((slide) => slide === nextSlide);
 
-  moveToSlide(track, currentSlide, nextSlide);
-  updateDots(currentDot, nextDot);
-  hideShowArrows(slides, prevButton, nextButton, nextIndex);
-});
+//   moveToSlide(track, currentSlide, nextSlide);
+//   updateDots(currentDot, nextDot);
+//   hideShowArrows(slides, prevButton, nextButton, nextIndex);
+// });
 
-// when i click the nav indicators move to that slide
-dotsNav.addEventListener("click", (e) => {
-  // what did we click on
-  const targetDot = e.target.closest("button");
-  if (!targetDot) return;
+// // when i click the nav indicators move to that slide
+// dotsNav.addEventListener("click", (e) => {
+//   // what did we click on
+//   console.log("hello");
+//   const targetDot = e.target.closest("button");
+//   if (!targetDot) return;
 
-  const currentSlide = track.querySelector(".current-slide");
-  const currentDot = dotsNav.querySelector(".current-slide");
-  const targetIndex = dots.findIndex((dot) => dot === targetDot);
-  const targetSlide = slides[targetIndex];
-  moveToSlide(track, currentSlide, targetSlide);
-  updateDots(currentDot, targetDot);
+//   const currentSlide = track.querySelector(".current-slide");
+//   const currentDot = dotsNav.querySelector(".current-slide");
+//   const targetIndex = dots.findIndex((dot) => dot === targetDot);
+//   const targetSlide = slides[targetIndex];
+//   moveToSlide(track, currentSlide, targetSlide);
+//   updateDots(currentDot, targetDot);
 
-  hideShowArrows(slides, prevButton, nextButton, targetIndex);
-});
-
-// const testDiv = document.getElementById("test-div");
-// const testButton = document.getElementById("test-button");
-
-// testButton.addEventListener("click", (event) => {
-//   // const image = new Image(600, 400);
-//   // image.src = "project-images-and-gifs/diagon-alley-cart-and-checkout.png";
-//   // testDiv.appendChild(image);
-//   console.log(testButton.innerHTML);
-//   if (testButton.innerText === "Diagon Alley") {
-
-//     const image2 = new Image(600, 400);
-//     image2.src = "project-images-and-gifs/diagon-alley-cart-and-checkout.png";
-//     testDiv.appendChild(image2);
-//     const image3 = new Image(600, 400);
-//     image3.src = "project-images-and-gifs/diagon-alley-cart-and-checkout.png";
-//     testDiv.appendChild(image3);
-//   }
+//   hideShowArrows(slides, prevButton, nextButton, targetIndex);
 // });
